fc6f0a2927a27d4da00828586cb31e074db5e764
Gareth Davis <gdavis@softwareinstitute.com>
2021-08-23 11:11:30 +0100

    Maven and sonarcloud

---
+++ src/test/java/TestPlayer.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class TestPlayer {
+    @Test
+    public void testGetName(){
+        assertEquals(10,10,"This is a rubbish test");
+    }
+
+    @Test
+    public void testSomethingElse(){
+        String name ="Ash";
+        assertSame(name,name,"This was not the same");
+    }
+
+
+}
