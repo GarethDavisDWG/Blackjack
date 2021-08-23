fe20225f07a05ff1b5b3afd497b787b9d437c0b0
Gareth Davis <gdavis@softwareinstitute.com>
2021-08-23 14:02:22 +0100

    on java 16

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,27 +1,11 @@
 package sample;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main //extends Application
 {
 
- /*   @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }*/
-
-
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         //Card card = new Card(Suit.SPADES,Value.ACE);
         //Deck deck = new Deck();
         System.out.print(Shuffle.shuffle(3));
